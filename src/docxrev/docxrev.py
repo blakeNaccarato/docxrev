74bac3d00e5078ca60782e99a8429a04b981a0a1
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-10-16 16:17:35 -0700

    Pass through `*_on_exit` params to Document

--- src/docxrev/docxrev.py
+++ src/docxrev/docxrev.py
@@ -1,22 +1,24 @@
-"""
-Microsoft Word review tools (comments, markup, etc.) with Python
-"""
+"""Microsoft Word review tools (comments, markup, etc.) with Python."""
 
-from docxrev import com
-from docxrev.com import Document
+from typing import Optional
+
+from . import com
+from .com import Document
 
 
 def quit_word():
     """Quit Word."""
-    com.WORD.Quit()
+    com.COM_WORD.Quit()
 
 
 def quit_word_safely():
     """Quit Word if no documents are open."""
-    if not com.WORD.Documents:
+    if not com.COM_WORD.Documents:
         quit_word()
 
 
-def get_active_document() -> Document:
+def get_active_document(
+    save_on_exit: bool = True, close_on_exit: Optional[bool] = None
+) -> Document:
     """Get the current document."""
-    return Document(com.WORD.ActiveDocument.FullName)
+    return Document(com.COM_WORD.ActiveDocument.FullName, save_on_exit, close_on_exit)
